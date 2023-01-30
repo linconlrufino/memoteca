edbc76fa350af587cd14233a7c5a764888804e70
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-01-30 19:27:39 -0300

    aula5.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
@@ -21,11 +22,15 @@ const routes: Routes = [
   {
     path: 'pensamentos/excluirPensamento/:id',
     component:ExcluirPensamentoComponent
+  },
+  {
+    path:'pensamentos/editarPensamento/:id',
+    component:EditarPensamentoComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
