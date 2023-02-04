07b5028b1dfda5c83c2084678fab737711d7cf69
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-02-03 23:42:16 -0300

    aula10.1

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -12,7 +12,8 @@ export class PensamentoComponent implements OnInit {
     id:0 ,
     conteudo: 'I love Angular',
     autoria: 'Developer',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
 
   constructor() { }
@@ -28,4 +29,10 @@ export class PensamentoComponent implements OnInit {
     return 'pensamento-p'
   }
 
+  mudarIconeFavorito() : string {
+    if(this.pensamento.favorito == false)
+      return 'inativo';
+
+      return 'ativo';
+  }
 }
